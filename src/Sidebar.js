39c8c1b599902d8d62dadc52d454da315b2c1872
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-12 17:45:19 -0500

    finished the sidebar component with styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import AddIcon from '@material-ui/icons/Add'
 import SidebarChannel from './SidebarChannel';
-import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import { Avatar } from '@material-ui/core';
+import { ExpandMore, Add, SignalCellularAlt, Call, InfoOutlined, Headset, Mic, Settings } from '@material-ui/icons';
 
 function Sidebar() {
     return (
@@ -10,18 +9,18 @@ function Sidebar() {
             <div className='sidebar__top'>
                 <h3>Kevin</h3>
 
-                <ExpandMoreIcon/>
+                <ExpandMore/>
             </div>
 
             <div className="sidebar__channels">
                 <div className="sidebar__channels-header">
                     <div className="sidebar__header">
-                        <ExpandMoreIcon/>
+                        <ExpandMore/>
 
                         <h4>Text Channel </h4>
                     </div>
 
-                    <AddIcon className='sidebar__addChannel'/>
+                    <Add className='sidebar__addChannel'/>
                 </div>
 
                 <div className="sidebar__channels-list">
@@ -33,12 +32,31 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__voice">
-                <SignalCellularAltIcon className='sidebar__voice-icon' fontSize='large' />
+                <SignalCellularAlt className='sidebar__voice-icon' fontSize='large' />
 
                 <div className="sidebar__voice-info">
                     <h3>Voice Connected</h3>
 
                     <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voice-icons">
+                    <InfoOutlined />
+                    <Call />
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar />
+                <div className="sidebar__profile-info">
+                    <h3>Kevin Castro</h3>
+                    <p>This is ID</p>
+                </div>
+
+                <div className="sidebar__profile-icons">
+                    <Mic/>
+                    <Headset/>
+                    <Settings/>
                 </div>
             </div>
         </div>
